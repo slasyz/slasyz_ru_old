bf9e267034655713d7fc7b4a723d0237110b89a6
Vyacheslav S <slasyz@gmail.com>
2014-02-07 16:24:10 +0400

    fixed error pages

--- slasyz_ru/views.py
+++ slasyz_ru/views.py
@@ -15,13 +15,13 @@ def index(request):
 	return HttpResponse(render(request, 'index.html', {'color': color}))
 
 def custom_400(request):
-    return HttpResponse(render(request, 'error.html', {'code': 400, 'name': 'Bad Request'}))
+    return HttpResponse(render(request, 'error.html', {'code': 400, 'name': 'Bad Request'}), status=400)
 
 def custom_403(request):
-    return HttpResponse(render(request, 'error.html', {'code': 403, 'name': 'Forbidden'}))
+    return HttpResponse(render(request, 'error.html', {'code': 403, 'name': 'Forbidden'}), status=403)
 
 def custom_404(request):
-    return HttpResponse(render(request, 'error.html', {'code': 404, 'name': 'Not Found'}))
+    return HttpResponse(render(request, 'error.html', {'code': 404, 'name': 'Not Found'}), status=404)
 
 def custom_500(request):
-    return HttpResponse(render(request, 'error.html', {'code': 500, 'name': 'Internal Server Error'}))
+    return HttpResponse(render(request, 'error.html', {'code': 500, 'name': 'Internal Server Error'}), status=404)

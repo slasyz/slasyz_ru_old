85273033d777e0779fd24264a2c020a6767c4baf
Vyacheslav S <slasyz@gmail.com>
2014-03-05 18:53:47 +0400

    removed deprecated sh^Wstuff

--- slasyz_ru/views.py
+++ slasyz_ru/views.py
@@ -7,13 +7,7 @@ from django.template import Context
 from django.shortcuts import render
 
 def index(request):
-	from random import choice
-
-	bgcolors =   ('#002', '#020', '#200', '#202', '#022', '#222')
-	textcolors = ('#bbf', '#bfb', '#fbb', '#fbf', '#bff', '#bbb')
-
-	color = {'background': choice(bgcolors), 'text': choice(textcolors)}
-	return HttpResponse(render(request, 'index.html', {'color': color}))
+	return HttpResponse(render(request, 'index.html', {}))
 
 def custom_400(request):
     return HttpResponse(render(request, 'error.html', {'code': 400, 'name': 'Bad Request'}), status=400)

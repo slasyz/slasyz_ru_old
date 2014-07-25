8239e70adf79679672eb1c781f77fa0002154a8f
Vyacheslav S <slasyz@gmail.com>
2014-07-25 10:43:50 +0400

    small RedirectIfAnonymous middleware fix

--- slasyz_ru/middleware.py
+++ slasyz_ru/middleware.py
@@ -5,12 +5,15 @@ from slasyz_ru.settings import INFO
 class RedirectIfAnonymous:
     def process_request(self, request):
         url = resolve(request.path)
+        APP_INFO = dict(INFO).get(url.app_name)
 
         if url.url_name == 'login':
             return None # do not redirect if it is a login page
-        if dict(INFO)[url.app_name]['needs_admin']:
-            if request.user.is_authenticated():
-                if not request.user.is_superuser:
-                    return # error: authenticated but hasn't admin rights
-            else:
-                return HttpResponseRedirect(reverse('login'))
+
+        if APP_INFO:
+            if APP_INFO['needs_admin']:
+                if request.user.is_authenticated():
+                    if not request.user.is_superuser:
+                        return # error: authenticated but hasn't admin rights
+                else:
+                    return HttpResponseRedirect(reverse('login'))

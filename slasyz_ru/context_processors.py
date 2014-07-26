e399cc41842a7c75f8c46ef6cd430b5da8c957fe
Vyacheslav Syrovatsky <slasyz@gmail.com>
2014-07-26 17:07:46 +0400

    fixed context processor

--- slasyz_ru/context_processors.py
+++ slasyz_ru/context_processors.py
@@ -1,8 +1,11 @@
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, Resolver404
 from slasyz_ru.settings import APPS
 
 def default(request):
-    APP_NAME = resolve(request.path).app_name
-    return {'APP_NAME': APP_NAME,
-            'APP_INFO': dict(APPS).get(APP_NAME),
-            'APPS': APPS}
+    try:
+        APP_NAME = resolve(request.path).app_name
+        return {'APP_NAME': APP_NAME,
+                'APP_INFO': dict(APPS).get(APP_NAME),
+                'APPS': APPS}
+    except Resolver404:
+        return {'APPS': APPS}

cb4a967b418973e8b9d34a6ccd259e14e9d443c5
Vyacheslav Syrovatsky <slasyz@gmail.com>
2014-07-26 14:15:47 +0400

    added CommentAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 #@admin.register(Post) # in dev-version
 class PostAdmin(admin.ModelAdmin):
     ordering = ['-created']
     list_display = ['title', 'author', 'created', 'get_absolute_url', 'get_comments_count', 'annotation']
 
+class CommentAdmin(admin.ModelAdmin):
+    ordering = ['created']
+    list_display = ['post', 'author', 'author_name', 'created', 'get_absolute_url', 'text']
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
